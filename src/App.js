8b38eba5ba04eee626b0b907e77c99ad31162dcd
Jon Schwab <jon@integritystl.com>
2016-08-07 21:51:11 -0500

    Adding starting branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 
-import ItemList from './ItemList';
-// import logo from './logo.svg';
 import './App.css';
-import data from './data';
 
 class App extends Component {
   constructor() {
@@ -18,34 +15,15 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    this.refs.newItem.focus();
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const newItem = {
-      id: this.state.items.length + 1,
-      title: this.refs.newItem.value,
-    }
-    this.setState({ items: [...this.state.items, newItem], });
-  }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          {/*<img src={logo} className="App-logo" alt="logo" /> */}
-          <h1>{data.title}</h1>
+          <h1>Title goes here</h1>
         </div>
 
-        <ItemList items={this.state.items} />
-
-        <form onSubmit={this.handleSubmit} className="item-form">
-          <input ref="newItem" />
-          <input type="submit" value="Add" />
-        </form>
-
+        {/* form class = item-form */}
       </div>
     );
   }
--- src/Item.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class ItemList extends Component {
-  render() {
-    return (
-      <li><a href={`/item/${this.props.id}`}>{this.props.title}</a></li>
-    );
-  }
-}
